6ffc22ab71b3b9fb9618d2b345be1962b53f9075
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 00:54:31 +0530

    Update ShoppingCart.java

--- src/core/ShoppingCart.java
+++ src/core/ShoppingCart.java
@@ -1,4 +1,20 @@
-package core;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingCart {
-}
+    private List<Product> products;
+
+    public ShoppingCart() {
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public double calculateTotalCost() {
+        double total = 0;
