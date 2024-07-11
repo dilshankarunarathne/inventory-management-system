7910832a3bd7e7d500cf3435d8cd9eb598ecf938
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 01:05:03 +0530

    Update ShoppingManager.java

--- src/core/ShoppingManager.java
+++ src/core/ShoppingManager.java
@@ -1,16 +1,4 @@
 package core;
 
 public interface ShoppingManager {
-    void addCart(ShoppingCart cart);
-    void removeCart(ShoppingCart cart);
-    ShoppingCart getCartByUserId(String userId);
-    void loginUser(String userId);
-    void logoutUser(String userId);
-    boolean checkProductAvailability(String productId, int quantity);
-    void processPayment(String userId, PaymentDetails paymentDetails);
-    void addProduct(Product product);
-    void updateProduct(String productId, Product updatedProduct);
-    void removeProduct(String productId);
-    OrderStatus trackOrder(String orderId);
-    boolean handleReturnOrExchange(String orderId, String productId, ReturnOrExchangeDetails details);
 }
