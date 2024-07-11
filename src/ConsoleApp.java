d5d28dd507c66a0c969017e9bdb80a1551d2d57b
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 01:09:47 +0530

    Update ConsoleApp.java

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -1,5 +1,6 @@
 public class ConsoleApp {
     public static void main(String[] args) {
-        
+        WestminsterShoppingManager manager = new WestminsterShoppingManager();
+        manager.displayMenu();
     }
 }
