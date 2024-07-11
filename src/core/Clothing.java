3fddeb1eea79e00c9a17ebe0e608c53c8180e194
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 00:53:55 +0530

    Create Clothing.java

---
+++ src/core/Clothing.java
@@ -0,0 +1,26 @@
+public class Clothing extends Product {
+    private String size;
+    private String color;
+
+    public Clothing(String productId, String productName, int availableItems, double price, String size, String color) {
+        super(productId, productName, availableItems, price);
+        this.size = size;
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
