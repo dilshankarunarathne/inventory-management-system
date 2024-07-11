56b117806a2719e4be53526835448f909fe918c8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 01:31:48 +0530

    Update ConsoleApplication.java

--- src/ConsoleApplication.java
+++ src/ConsoleApplication.java
@@ -1,6 +1,6 @@
 import core.WestminsterShoppingManager;
 
-public class MainWindow {
+public class ConsoleApplication {
     public static void main(String[] args) {
         WestminsterShoppingManager manager = new WestminsterShoppingManager();
         manager.displayMenu();
