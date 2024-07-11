f531e3473c9737fb600871d9b8475624995a01cf
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 01:16:30 +0530

    Update Product.java

--- src/core/Product.java
+++ src/core/Product.java
@@ -46,8 +46,5 @@ public abstract class Product {
     }
 
     @Override
-    public String toString() {
-        return "Clothing [" + getProductId() + ": " + getProductName() + ", avail=" + getAvailableItems() +
-                ", price=" + getPrice() + ']';
-    }
+    public abstract String toString();
 }
