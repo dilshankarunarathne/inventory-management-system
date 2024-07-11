8c001c7bfae5f8c559864e84c2d857e9e04082f3
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 00:53:15 +0530

    Update Product.java

--- src/core/Product.java
+++ src/core/Product.java
@@ -1,4 +1,45 @@
-package core;
+public abstract class Product {
+    private String productId;
+    private String productName;
+    private int availableItems;
+    private double price;
 
-public class Product {
+    public Product(String productId, String productName, int availableItems, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.availableItems = availableItems;
+        this.price = price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getAvailableItems() {
+        return availableItems;
+    }
+
+    public void setAvailableItems(int availableItems) {
+        this.availableItems = availableItems;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
