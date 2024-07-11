e1861ff9570930b7050639d945a71bcb085092c8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 01:06:56 +0530

    Update ShoppingManager.java

--- src/core/ShoppingManager.java
+++ src/core/ShoppingManager.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public interface ShoppingManager {
     void displayMenu();
-    void addProduct(Scanner scanner);
     void deleteProduct(Scanner scanner);
     void printProducts();
     void saveProducts();
