47d20520f49061c8e08645e68b27de4ebeec653f
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-12 01:30:23 +0530

    Create MainWindow.java

---
+++ src/MainWindow.java
@@ -0,0 +1,8 @@
+import core.WestminsterShoppingManager;
+
+public class MainWindow {
+    public static void main(String[] args) {
+        WestminsterShoppingManager manager = new WestminsterShoppingManager();
+        manager.displayMenu();
+    }
+}
