b4a8b05af6a8edd4e0e40d128248474811f49a21
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-07-15 03:33:07 +0530

    Update ShoppingCart.java

--- src/core/ShoppingCart.java
+++ src/core/ShoppingCart.java
@@ -15,6 +15,7 @@ public class ShoppingCart {
         this.isFirstPurchase = true; // Assuming this is the first purchase by default
     }
 
+    // Add and remove products from the cart
     public void addProduct(Product product) {
         products.add(product);
     }
